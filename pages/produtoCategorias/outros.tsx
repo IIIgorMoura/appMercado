0942ea62803ed74a57832c18e027745e0173fbbb
IIIgorMoura <133662971+IIIgorMoura@users.noreply.github.com>
2024-05-22 00:57:29 -0300

    Adicao de remocao de itens de Produtos

--- pages/produtoCategorias/outros.tsx
+++ pages/produtoCategorias/outros.tsx
@@ -1,35 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { obterProdutos } from '../../hooks/bancoProduto';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { obterProdutos, removerProduto } from '../../hooks/bancoProduto';
+import { Ionicons } from '@expo/vector-icons';
 import ESTILOS from '../../styles/ESTILOS';
 
 export function Outros() {
   const [produtos, setProdutos] = useState([]);
 
-  useEffect(() => {
-    const carregarProdutos = async () => {
-      try {
-        const todosProdutos = await obterProdutos();
-        const produtosOutros = todosProdutos.filter(produto => produto.tipo === 'outros');
-        setProdutos(produtosOutros);
-      } catch (error) {
-        console.error('Erro ao carregar produtos: ', error);
-      }
-    };
+  const carregarProdutos = async () => {
+    try {
+      const todosProdutos = await obterProdutos();
+      const produtosOutros = todosProdutos.filter(produto => produto.tipo === 'outros');
+      setProdutos(produtosOutros);
+    } catch (error) {
+      console.error('Erro ao carregar produtos: ', error);
+    }
+  };
 
+  useEffect(() => {
     carregarProdutos();
   }, []);
 
+  const handleRemoveProduto = async (id) => {
+    try {
+      await removerProduto(id);
+      carregarProdutos();
+    } catch (error) {
+      console.error('Erro ao remover produto: ', error);
+    }
+  };
+
   const renderItem = ({ item }) => (
     <View style={styles.item}>
-      <Text style={styles.title}>{item.nome}</Text>
-      <Text>{`Preço: R$ ${item.preco.toFixed(2)}`}</Text>
+      <View style={styles.itemInfo}>
+        <Text style={styles.title}>{item.nome}</Text>
+        <Text>{`Preço: R$ ${item.preco.toFixed(2)}`}</Text>
+      </View>
+      <TouchableOpacity onPress={() => handleRemoveProduto(item.id)}>
+        <Ionicons name="trash-bin-outline" size={24} color="red" />
+      </TouchableOpacity>
     </View>
   );
 
   return (
     <View style={ESTILOS.container}>
-      <Text>Outros</Text>
+      <Text style={styles.header}>Outros</Text>
       <FlatList
         data={produtos}
         renderItem={renderItem}
@@ -40,10 +55,21 @@ export function Outros() {
 }
 
 const styles = StyleSheet.create({
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
   item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
+  },
+  itemInfo: {
+    flexDirection: 'column',
   },
   title: {
     fontSize: 24,
